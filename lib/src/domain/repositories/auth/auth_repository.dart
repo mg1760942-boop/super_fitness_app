4c173a360c887e1e134a6608f71089f3c89510ac
alisafwat <alisafwat11@gmail.com>
2025-02-15 20:57:05 +0200

    Implement AuthRepository interface and its implementation for password management

---
+++ lib/src/domain/repositories/auth/auth_repository.dart
@@ -0,0 +1,13 @@
+import 'package:super_fitness_app/core/common/apis/api_result.dart';
+import 'package:super_fitness_app/src/domain/entities/auth/forget_password/forget_password_request_entity.dart';
+import 'package:super_fitness_app/src/domain/entities/auth/forget_password/forget_password_response_entity.dart';
+import 'package:super_fitness_app/src/domain/entities/auth/forget_password/reset_password_request_entity.dart';
+import 'package:super_fitness_app/src/domain/entities/auth/forget_password/reset_password_response_entity.dart';
+import 'package:super_fitness_app/src/domain/entities/auth/forget_password/verify_reset_code_request_entity.dart';
+import 'package:super_fitness_app/src/domain/entities/auth/forget_password/verify_reset_code_response_entity.dart';
+
+abstract interface class AuthRepository{
+  Future<ApiResult<ForgetPasswordResponseEntity>> forgetPassword(ForgetPasswordRequestEntity request);
+  Future<ApiResult<VerifyResetCodeResponseEntity>> verifyResetCode(VerifyResetCodeRequestEntity request);
+  Future<ApiResult<ResetPasswordResponseEntity>> resetPassword(ResetPasswordRequestEntity request);
+}

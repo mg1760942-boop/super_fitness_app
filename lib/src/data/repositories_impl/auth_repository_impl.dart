a3f0bf8b17d044dec3c5c8c0a38451e22fd23db7
Your Name <omarhamed.workl@example.com>
2025-02-17 06:40:54 +0200

    Implement saving token to offline data source in AuthRepositoryImpl
    - Add AuthOfflineDataSource dependency to AuthRepositoryImpl
    - Save token using AuthOfflineDataSource after successful registration

--- lib/src/data/repositories_impl/auth_repository_impl.dart
+++ lib/src/data/repositories_impl/auth_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:injectable/injectable.dart';
 import 'package:super_fitness_app/core/common/apis/api_executer.dart';
 import 'package:super_fitness_app/core/common/apis/api_result.dart';
 import 'package:super_fitness_app/src/api/core/api_request_models/register_request_model/register_request_model.dart';
+import 'package:super_fitness_app/src/data/data_source/offline_data_source/auth/auth_offline_data_source/auth_offline_data_source.dart';
 import 'package:super_fitness_app/src/domain/entities/app_user_entity/app_user_entity.dart';
 import 'package:super_fitness_app/src/domain/repositories/auth/auth_repository.dart';
 
@@ -10,11 +11,13 @@ import '../data_source/online_data_source/auth/auth_online_data_source.dart';
 @Injectable(as:AuthRepository )
 class AuthRepositoryImpl implements AuthRepository{
   final AuthOnlineDataSource _authOnlineDataSource;
-  AuthRepositoryImpl( this._authOnlineDataSource);
+  final AuthOfflineDataSource _authOfflineDataSource;
+  AuthRepositoryImpl( this._authOnlineDataSource,this._authOfflineDataSource);
   @override
   Future<ApiResult<AppUserEntity>> register({required RegisterRequestModel registerRequest}) async{
     return executeApi<AppUserEntity>(apiCall: ()async {
       var response =await _authOnlineDataSource.register(registerRequest);
+      var authOfflineDataSource=await _authOfflineDataSource.saveToken(token: response.$2);
       return response.$1;
     },);
   }

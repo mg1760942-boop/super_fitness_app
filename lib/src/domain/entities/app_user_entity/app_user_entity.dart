8ceb9e54ca7ad7f8b8cf7c5fdb640a13b6699c27
Your Name <omarhamed.workl@example.com>
2025-02-17 07:13:01 +0200

    Implement RegisterUseCase and update dependency injection
    - Add `RegisterUseCase` for handling user registration.
    - Update dependency injection to include `RegisterUseCase`.
    - Update AuthRepositoryImpl to include AuthOfflineDataSource.

--- lib/src/domain/entities/app_user_entity/app_user_entity.dart
+++ lib/src/domain/entities/app_user_entity/app_user_entity.dart
@@ -1,25 +1,23 @@
 class AppUserEntity {
-  final String ? firstName;
-  final String ? lastName;
-  final String ?  email;
-  final  String ? gender;
-  final num ?  height;
-  final num ? weight;
-  final num ? age;
-  final String ?  goal;
-  final String ? activityLevel;
+  final String? firstName;
+  final String? lastName;
+  final String? email;
+  final String? gender;
+  final num? height;
+  final num? weight;
+  final num? age;
+  final String? goal;
+  final String? activityLevel;
 
-
- AppUserEntity({
+  AppUserEntity({
     required this.firstName,
-   required this.lastName,
-   required this.email,
-   required this.gender,
-   required this.height,
-   required this.weight,
-   required this.age,
-   required this.goal,
-   required this.activityLevel,
-
-});
+    required this.lastName,
+    required this.email,
+    required this.gender,
+    required this.height,
+    required this.weight,
+    required this.age,
+    required this.goal,
+    required this.activityLevel,
+  });
 }

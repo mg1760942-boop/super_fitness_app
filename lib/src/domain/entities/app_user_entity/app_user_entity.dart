628bb0408a408ce1d4d57c1802d354d1ec9df592
Your Name <omarhamed.workl@example.com>
2025-02-17 06:11:50 +0200

    Generate ApiServices with Retrofit and add AppUserEntity
    - Generate `api_services.g.dart` for API service interactions using Retrofit.
    - Add `AppUserEntity` with user attributes.

---
+++ lib/src/domain/entities/app_user_entity/app_user_entity.dart
@@ -0,0 +1,25 @@
+class AppUserEntity {
+  final String ? firstName;
+  final String ? lastName;
+  final String ?  email;
+  final  String ? gender;
+  final double ?  height;
+  final double ? weight;
+  final double ? age;
+  final String ?  goal;
+  final String ? activityLevel;
+
+
+ AppUserEntity({
+    required this.firstName,
+   required this.lastName,
+   required this.email,
+   required this.gender,
+   required this.height,
+   required this.weight,
+   required this.age,
+   required this.goal,
+   required this.activityLevel,
+
+});
+}

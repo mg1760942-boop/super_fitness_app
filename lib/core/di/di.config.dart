3eaf8e6018bc23831135b2556adc5e54e0977804
Your Name <omarhamed.workl@example.com>
2025-02-17 05:59:26 +0200

    Implement dependency injection and register API service
    - Create `di.config.dart` for dependency injection configuration using `InjectableConfigGenerator`.
    - Create `register_response_model.g.dart` for register response models with JsonSerializableGenerator.
    - Update `api_services.dart` to include a register method for handling register API calls.

---
+++ lib/core/di/di.config.dart
@@ -0,0 +1,50 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i361;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i558;
+import 'package:get_it/get_it.dart' as _i174;
+import 'package:injectable/injectable.dart' as _i526;
+import 'package:pretty_dio_logger/pretty_dio_logger.dart' as _i528;
+import 'package:shared_preferences/shared_preferences.dart' as _i460;
+
+import '../../src/api/api_services.dart' as _i587;
+import '../../src/api/network_factory.dart' as _i1041;
+import '../helpers/shared_pref/shared_pref_moduel.dart' as _i802;
+
+extension GetItInjectableX on _i174.GetIt {
+// initializes the registration of main-scope dependencies inside of GetIt
+  Future<_i174.GetIt> init({
+    String? environment,
+    _i526.EnvironmentFilter? environmentFilter,
+  }) async {
+    final gh = _i526.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    final sharedPrefModule = _$SharedPrefModule();
+    final dioProvider = _$DioProvider();
+    await gh.factoryAsync<_i460.SharedPreferences>(
+      () => sharedPrefModule.sharedPreferences,
+      preResolve: true,
+    );
+    gh.lazySingleton<_i558.FlutterSecureStorage>(
+        () => sharedPrefModule.secureStorage);
+    gh.lazySingleton<_i361.Dio>(() => dioProvider.dioProvider());
+    gh.lazySingleton<_i528.PrettyDioLogger>(() => dioProvider.providePretty());
+    gh.singleton<_i587.ApiServices>(() => _i587.ApiServices(gh<_i361.Dio>()));
+    return this;
+  }
+}
+
+class _$SharedPrefModule extends _i802.SharedPrefModule {}
+
+class _$DioProvider extends _i1041.DioProvider {}
--- lib/src/api/api_services.dart
+++ lib/src/api/api_services.dart
@@ -2,7 +2,10 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
+import 'package:super_fitness_app/src/api/core/api_response_models/register_respone_model/register_response_model.dart';
 import 'package:super_fitness_app/src/api/core/constants/api_base_url.dart';
+
+import 'core/api_request_models/register_request_model/register_request_model.dart';
 
 part 'api_services.g.dart';
 
@@ -13,7 +16,7 @@ abstract interface class ApiServices {
   @factoryMethod
   factory ApiServices(Dio dio) = _ApiServices;
 
-
+  Future<RegisterResponseModel>register({@Body() required RegisterRequestModel registerRequestModel});
 
 
 }

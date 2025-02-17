32bd26ad9269e8ec297c1a1896cf918757c1d393
Your Name <omarhamed.workl@example.com>
2025-02-17 05:56:34 +0200

    Refactor: Implement Dependency Injection and Add Register API Models
    -   Removed `api_services.g.dart` as it's no longer needed with the updated dependency injection.
    -   Implemented dependency injection using `get_it` and `injectable` packages.
    -   Added `di.dart` and `di.config.dart` files for dependency injection configuration.
    -   Added `RegisterRequestModel` for structuring register requests.
    -   Added `RegisterResponseModel` and `User` models for parsing register responses, using `json_annotation`.
    - Updated `main.dart` to initialize dependency injection.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:super_fitness_app/core/di/di.dart';
 import 'package:super_fitness_app/super_ditness_app.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+ await configureDependencies();
   runApp(const SuperFitnessApp());
 }
 

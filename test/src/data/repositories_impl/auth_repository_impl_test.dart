580471f6601a46cd6bf7e2557851a26738f15799
alisafwat <alisafwat11@gmail.com>
2025-02-20 20:57:52 +0200

    test reset password respository function

--- test/src/data/repositories_impl/auth_repository_impl_test.dart
+++ test/src/data/repositories_impl/auth_repository_impl_test.dart
@@ -4,6 +4,7 @@ import 'package:mockito/mockito.dart';
 import 'package:super_fitness_app/core/common/apis/api_result.dart';
 import 'package:super_fitness_app/src/data/api/core/api_request_models/login/login_request.dart';
 import 'package:super_fitness_app/src/data/api/core/api_response_models/forget_password/forget_password_response_model.dart';
+import 'package:super_fitness_app/src/data/api/core/api_response_models/forget_password/reset_password_response_model.dart';
 import 'package:super_fitness_app/src/data/api/core/api_response_models/forget_password/verify_reset_code_response_model.dart';
 import 'package:super_fitness_app/src/data/api/core/api_response_models/login/login_response.dart';
 import 'package:super_fitness_app/src/data/data_source/offline_data_source/auth/auth_offline_data_source/auth_offline_data_source.dart';
@@ -11,6 +12,8 @@ import 'package:super_fitness_app/src/data/data_source/online_data_source/auth/a
 import 'package:super_fitness_app/src/data/repository/auth/auth_repository_impl.dart';
 import 'package:super_fitness_app/src/domain/entities/auth/forget_password/forget_password_request_entity.dart';
 import 'package:super_fitness_app/src/domain/entities/auth/forget_password/forget_password_response_entity.dart';
+import 'package:super_fitness_app/src/domain/entities/auth/forget_password/reset_password_request_entity.dart';
+import 'package:super_fitness_app/src/domain/entities/auth/forget_password/reset_password_response_entity.dart';
 import 'package:super_fitness_app/src/domain/entities/auth/forget_password/verify_reset_code_request_entity.dart';
 import 'package:super_fitness_app/src/domain/entities/auth/forget_password/verify_reset_code_response_entity.dart';
 
@@ -180,6 +183,57 @@ void main() {
 
       expect(result, isA<Failures<VerifyResetCodeResponseEntity>>());
       verify(authOnlineDataSource.verifyResetCode(any)).called(1);
+    });
+  });
+  group("ResetPassword function", () {
+    const email = "fackemail@fake.com";
+    const password = "newPassword";
+    final request = ResetPasswordRequestEntity(email: email,newPassword: password);
+    final mockResponse = ResetPasswordResponseEntity();
+    final onlineDataResponse = ResetPasswordResponseModel();
+    final successResult = Success<ResetPasswordResponseEntity>(data: mockResponse);
+    final failureResult = Failures<ResetPasswordResponseEntity>(exception: Exception("API Error"));
+
+    test("should return Success when API call is successful", () async {
+      provideDummy<ApiResult<ResetPasswordResponseEntity>>(successResult);
+      when(authOnlineDataSource.resetPassword(any))
+          .thenAnswer((_) async => onlineDataResponse);
+
+      final result = await authRepositoryImpl.resetPassword(request);
+
+      expect(result, isA<Success<ResetPasswordResponseEntity>>());
+      verify(authOnlineDataSource.resetPassword(any)).called(1);
+    });
+
+    test("should return Failure when API call fails", () async {
+      provideDummy<ApiResult<ResetPasswordResponseEntity>>(failureResult);
+      when(authOnlineDataSource.resetPassword(any))
+          .thenThrow(Exception("API Error"));
+
+      final result = await authRepositoryImpl.resetPassword(request);
+
+      expect(result, isA<Failures<ResetPasswordResponseEntity>>());
+      verify(authOnlineDataSource.resetPassword(any)).called(1);
+    });
+    test("should return Failure when API call fails because email is empty", () async {
+      provideDummy<ApiResult<ResetPasswordResponseEntity>>(failureResult);
+      when(authOnlineDataSource.resetPassword(any))
+          .thenThrow(Exception("email should not be empty"));
+
+      final result = await authRepositoryImpl.resetPassword(ResetPasswordRequestEntity(email: "",newPassword: password));
+
+      expect(result, isA<Failures<ResetPasswordResponseEntity>>());
+      verify(authOnlineDataSource.resetPassword(any)).called(1);
+    });
+    test("should return Failure when API call fails because password less than 8", () async {
+      provideDummy<ApiResult<ResetPasswordResponseEntity>>(failureResult);
+      when(authOnlineDataSource.resetPassword(any))
+          .thenThrow(Exception("weak password"));
+
+      final result = await authRepositoryImpl.resetPassword(ResetPasswordRequestEntity(email: "",newPassword: "123"));
+
+      expect(result, isA<Failures<ResetPasswordResponseEntity>>());
+      verify(authOnlineDataSource.resetPassword(any)).called(1);
     });
   });
 

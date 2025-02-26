fe07294efb270396b677f0d66fcee1c34d653a0d
Your Name <omarhamed.workl@example.com>
2025-02-26 14:10:42 +0200

    Refactor EditProfileView to use EditProfileAppBarWidget
    - Extract the app bar logic from `EditProfileView` into a new `EditProfileAppBarWidget`.
    - Replace the previous app bar implementation in `EditProfileView` with the new `EditProfileAppBarWidget`.
    - Create the widget to include the back button, title "Edit Profile", and an empty space.

--- lib/src/presentation/pages/edit_profile/view/edit_profile_view.dart
+++ lib/src/presentation/pages/edit_profile/view/edit_profile_view.dart
@@ -5,6 +5,7 @@ import '../../../../../core/utilities/style/app_icons.dart';
 import '../../../../../core/utilities/style/app_images.dart';
 import '../../../../../core/utilities/style/app_text_styles.dart';
 import '../../../shared/base_scaffold.dart';
+import '../widget/edit_profile_app_bar_widget.dart';
 
 class EditProfileView extends StatelessWidget {
   const EditProfileView({super.key});
@@ -17,35 +18,7 @@ class EditProfileView extends StatelessWidget {
         padding:  EdgeInsets.symmetric(vertical: 49.0.h,horizontal: 16.0.w),
         child: Column(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                SizedBox(
-                  width: 24,
-                  height: 24,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFFF4100),
-                      shape: BoxShape.circle,
-                    ),
-                    child: IconButton(
-                      padding: EdgeInsets.zero,
-                      constraints: const BoxConstraints(),
-                      icon: SvgPicture.asset(
-                        AppIcons.backIcon,
-                        color: Colors.white,
-                        width: 10,
-                        height: 10,
-                      ),
-                      onPressed: () {
-                      },
-                    ),
-                  )
-                ),
-                Text("Edit Profile",style: AppTextStyles.font20w600White,),
-                const SizedBox(width: 24),
-              ],
-            ),
+            EditProfileAppBarWidget(),
           ],
         ),
       ),
---
+++ lib/src/presentation/pages/edit_profile/widget/edit_profile_app_bar_widget.dart
@@ -0,0 +1,42 @@
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../../core/common/common_imports.dart';
+import '../../../../../core/utilities/style/app_icons.dart';
+import '../../../../../core/utilities/style/app_text_styles.dart';
+
+class EditProfileAppBarWidget extends StatelessWidget {
+  const EditProfileAppBarWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return         Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        SizedBox(
+            width: 24,
+            height: 24,
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xFFFF4100),
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(
+                padding: EdgeInsets.zero,
+                constraints: const BoxConstraints(),
+                icon: SvgPicture.asset(
+                  AppIcons.backIcon,
+                  color: Colors.white,
+                  width: 10,
+                  height: 10,
+                ),
+                onPressed: () {
+                },
+              ),
+            )
+        ),
+        Text("Edit Profile",style: AppTextStyles.font20w600White,),
+        const SizedBox(width: 24),
+      ],
+    );
+  }
+}

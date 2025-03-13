22bede86cc757680ea2cdd7c0f447aa2834d249f
alisafwat <alisafwat11@gmail.com>
2025-03-13 17:38:56 +0200

    add: get all execises method to workouts online datasource

--- lib/src/data/data_source/online_data_source/workouts/workouts_online_data_source.dart
+++ lib/src/data/data_source/online_data_source/workouts/workouts_online_data_source.dart
@@ -1,5 +1,8 @@
+import 'package:super_fitness_app/src/data/api/core/api_response_models/workouts/all_exercises_response_model.dart';
+
 import '../../../api/core/api_response_models/workouts/muscles_group_response_model.dart';
 
 abstract interface class WorkoutsOnlineDataSource {
   Future<MusclesGroupResponseModel> getMusclesGroup();
+  Future<AllExercisesResponseModel> getAllExercises();
 }
--- lib/src/data/data_source/online_data_source/workouts/workouts_online_data_source_impl.dart
+++ lib/src/data/data_source/online_data_source/workouts/workouts_online_data_source_impl.dart
@@ -1,6 +1,7 @@
 
 import 'package:injectable/injectable.dart';
 import 'package:super_fitness_app/src/data/api/api_services.dart';
+import 'package:super_fitness_app/src/data/api/core/api_response_models/workouts/all_exercises_response_model.dart';
 import 'package:super_fitness_app/src/data/api/core/api_response_models/workouts/muscles_group_response_model.dart';
 import 'package:super_fitness_app/src/data/data_source/online_data_source/workouts/workouts_online_data_source.dart';
 
@@ -12,6 +13,11 @@ class WorkoutsOnlineDataSourceImpl implements WorkoutsOnlineDataSource{
   @override
   Future<MusclesGroupResponseModel> getMusclesGroup() async{
     return await _apiServices.getMusclesGroup();
+  }
+
+  @override
+  Future<AllExercisesResponseModel> getAllExercises() async{
+    return await _apiServices.getAllExercises();
   }
 
 }

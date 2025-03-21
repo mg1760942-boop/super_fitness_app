86abbe07c271e9d515000f69d5cb490cf8753f0d
alisafwat <alisafwat11@gmail.com>
2025-03-21 16:49:25 +0200

    add: bloc consumer to handel screens builder and listener

--- lib/src/presentation/pages/exercise/exercise_screen.dart
+++ lib/src/presentation/pages/exercise/exercise_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:super_fitness_app/src/presentation/managers/exercise/exercise_screen_states.dart';
 
 import '../../../../core/common/common_imports.dart';
 import '../../../../core/di/di.dart';
@@ -6,15 +7,30 @@ import '../../managers/exercise/exercise_screen_view_model.dart';
 import 'exercise_screen_body.dart';
 
 class ExerciseScreen extends StatelessWidget {
-   ExerciseScreen({super.key});
-
+  ExerciseScreen({super.key});
 
   final viewModel = getIt<ExerciseScreenViewModel>();
+
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(create: (_)=>viewModel,child:
-      Scaffold(
-        body: ExerciseScreenBody(),
-      ),);
+    return BlocProvider(
+      create: (_) => viewModel,
+      child: BlocConsumer<ExerciseScreenViewModel, ExerciseScreenStates>(
+        builder: (context, state) {
+          return Scaffold(
+            body: ExerciseScreenBody(),
+          );
+        },
+        listener: (context, state) {
+          _handelListenerState(context,state);
+        },
+      ),
+    );
+  }
+
+  _handelListenerState(context,state){
+    if(state is NavigateToBackState){
+      Navigator.of(context).pop();
+    }
   }
 }
